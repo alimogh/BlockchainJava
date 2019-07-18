07e40f75af4815ba26a7d4cb6afb4e712d1024d3
Srinivas Ashok Kumar <ashok.srinivas@letsventure.com>
2019-07-18 14:05:56 +0530

    Simple Hashing code example completed.

---
+++ src/com/ashokslsk/blockchain/BlockApp.java
@@ -0,0 +1,11 @@
+package com.ashokslsk.blockchain;
+
+public class BlockApp {
+    public static void main(String[] args) {
+        System.out.println(SHA256Helper.hash("Ashok Kumar Srinivas"));
+    }
+}
+
+
+// Sample output
+// 9a24a805a98d7ed92c9f494954394f9f9c47df6c9fddbd31aa22682a2ed74ad5 - Hashvalue for the string
